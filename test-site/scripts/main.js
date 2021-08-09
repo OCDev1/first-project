080e90157bcb737c479b2b8b18f83de0cd8aa99c
OCDev1 <88517165+OCDev1@users.noreply.github.com>
2021-08-09 18:56:47 +0300

    Add files via upload

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -13,6 +13,7 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
     let myName = prompt('Please enter your name.');
+    
     if(!myName) {
       setUserName();
     } else {
